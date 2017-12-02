331414eb6a98d96f1b517a9a1ede25f8d5ae9cba
Vonx <ryanthonn@gmail.com>
2017-12-01 21:57:52 -0800

    trying something

--- inmotion_react/src/_redux/actions/action_InitialLoad.js
+++ inmotion_react/src/_redux/actions/action_InitialLoad.js
@@ -45,7 +45,7 @@ export default () => (dispatch) => {
             });
 
             const tasks = [];
-            tasksData.map(({id, subject, list_id, priority}) => {
+            tasksData.map(({id, subject, priority, list_id}) => {
                 const newTask = new Task({id, subject, priority});
                 newTask.list = lists[list_id];
                 tasks[id] = newTask;
